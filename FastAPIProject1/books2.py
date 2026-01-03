4d386367ebe93fedf8a4f83dde2d778e9b053697
Musawer Afzal <musawerafzal23@gmail.com>
2026-01-03 22:02:35 +0500

    Moving forward with proper project

---
+++ FastAPIProject1/books2.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codewithme', 'A book for chads', 5),
+    Book(2, 'Be Fast with FastAPI', 'codewithme', 'A great book', 5),
+    Book(3, 'Master Endpoints', 'codewithme', 'A awesome book', 5),
+    Book(4, 'HP1', 'Author1', 'A wonderful book', 2),
+    Book(5, 'HP2', 'Author2', 'A easy book', 3),
+    Book(6, 'HP3', 'Author3', 'A coding book, Duh', 1),
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request = Body()):
+    BOOKS.append(book_request)
+    return BOOKS
