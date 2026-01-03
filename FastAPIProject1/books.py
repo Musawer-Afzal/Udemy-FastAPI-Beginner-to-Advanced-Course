11667e5626ed1277db2757c5b064fd63db6226a7
Musawer Afzal <musawerafzal23@gmail.com>
2026-01-03 21:38:43 +0500

    Completed Methods in FastAPI

--- FastAPIProject1/books.py
+++ FastAPIProject1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -43,3 +43,27 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+
+
+@app.post("/books/create_book")
+async def create_book(new_book = Body()):
+    BOOKS.append(new_book)
+    return BOOKS
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+    return BOOKS
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+    return BOOKS
