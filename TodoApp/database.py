8d5b6b90b4e72838d58c81feca1d72b773854aed
Musawer Afzal <musawerafzal23@gmail.com>
2026-01-17 16:09:37 +0500

    Making relative pathing for testing

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,10 +1,18 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread': False})
+# SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+SQLALCHEMY_DATABASE_URL = (
+    'sqlite:///./todosapp.db'
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
